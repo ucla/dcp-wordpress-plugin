068e30b60977a4018c91fc628c52a04168dbd502
alex_tou <atou@stratcomm.ucla.edu>
2022-08-15 10:08:42 -0700

    output html to save

--- src/ribbon/save.js
+++ src/ribbon/save.js
@@ -1,4 +1,35 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
 
-export default function save() {
-    return null;
+import { RichText, InnerBlocks } from '@wordpress/block-editor';
+
+/**
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
+ * @param {Object} [props]           Properties passed from the editor.
+ * @param {Object} props.attributes
+ * @param {string} props.attributes.ribbonContent
+ * @param {string} props.attributes.highlight
+ * @param {string} props.className
+ * @return {WPElement} Element to render.
+ */
+export default function save({
+    attributes: { ribbonContent, highlight },
+	className,
+}) {
+    return (
+        <div className={`ribbon${highlight === 'highlight' ? ' ribbon--highlight' : ''}`}>
+            <RichText.Content
+                tagName={highlight === 'highlight' ? 'h3' : 'h2'}
+                value={ ribbonContent }
+            />
+        </div>
+    );
 };
