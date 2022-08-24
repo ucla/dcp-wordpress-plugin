a4ad957f1150913b6f718065b83216210085bb52
alex_tou <atou@stratcomm.ucla.edu>
2022-08-24 14:21:17 -0700

    give option to remove molecule, remove molecule for ribbon highlight

--- src/ribbon/save.js
+++ src/ribbon/save.js
@@ -21,11 +21,11 @@ import { RichText, InnerBlocks } from '@wordpress/block-editor';
  * @return {WPElement} Element to render.
  */
 export default function save({
-    attributes: { ribbonContent, highlight },
+    attributes: { ribbonContent, highlight, molecule },
 	className,
 }) {
     return (
-        <div className={`ribbon${highlight === 'highlight' ? ' ribbon--highlight' : ''}`}>
+        <div className={`ribbon${className ? ' ' + className : ''}${highlight === 'highlight' ? ' ribbon--highlight' : molecule === true ? ' has-molecule' : ''}`}>
             <RichText.Content
                 tagName={highlight === 'highlight' ? 'h3' : 'h2'}
                 value={ ribbonContent }
