c34b8b21c0fa65e00fa739e8eb20b780ce2166e7
alex_tou <atou@stratcomm.ucla.edu>
2022-09-06 15:45:28 -0700

    fix editor story padding

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-polyfill', 'wp-primitives'), 'version' => '9f46e564b3788b695fda86ed106b971c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-polyfill', 'wp-primitives'), 'version' => '41ecf45d05631a22e13b61c935cc3f49');
