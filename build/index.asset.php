aa19cc565dcd2cd7556d3822417ec8a914336ac0
alex_tou <atou@stratcomm.ucla.edu>
2022-10-11 15:47:44 -0700

    fix photo credit position

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-polyfill', 'wp-primitives'), 'version' => '69e0c1985db22891cb51c8d64ecb77c5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-polyfill', 'wp-primitives'), 'version' => 'b73ad82c411d616fd7447b38415b7699');
